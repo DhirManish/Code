2f2487d83d11fc6ddfa84cb5b26101715f7f5c37
DhirManish <manish.dhir4@gmail.com>
2018-08-04 01:21:56 +0530

    Printing the specific indexed value from array using method

---
+++ Code/ArayInit.java
@@ -0,0 +1,23 @@
+class Array {
+
+	int[] a = {1, 2};
+	
+	public void return_Array_Element(){
+		
+		System.out.println(a[1]);
+		
+	}
+
+}
+
+public class ArayInit {
+	
+	public static void main(String[] args){
+		
+		Array arraycon = new Array();
+	
+		arraycon.return_Array_Element();
+	
+	}
+	
+}
