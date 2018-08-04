1515dcfca71cea827d47cbc306df94f688ec55cb
DhirManish <manish.dhir4@gmail.com>
2018-08-05 00:28:15 +0530

    Resuabe method to reverse string

--- Code/Reusability.java
+++ Code/Reusability.java
@@ -1,7 +1,34 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
 class class_for_reuseable_method_string_operations_under_dev{
+
+	String s = "Hello";
+	
+	public void reverse(){
+		
+		byte[] strunderaction = s.getBytes();
+		
+		byte[] result = new byte [strunderaction.length];
+		
+		for(int i = 0; i < strunderaction.length; i++)
+			result[i] =strunderaction[strunderaction.length-i-1];
+		
+		System.out.println(new String(result));
+		
+	}
 
 }
 
 public class Reusability{
-
+	
+	public static void main(String[] args){
+		
+		class_for_reuseable_method_string_operations_under_dev c = new class_for_reuseable_method_string_operations_under_dev();
+		
+		c.reverse();
+		
+	}
+	
 }
