930b200243778dbce07693f3ee0286ffae2e97db
DhirManish <manish.dhir4@gmail.com>
2018-08-12 00:00:10 +0530

    Lemon Tea

--- Code/TeaMaker.java
+++ Code/TeaMaker.java
@@ -1,11 +1,27 @@
 interface tea{
 
-	String teaitems();
+	void prepare_tea();
 
+}
+
+class Lemontea implements tea{
+	
+	public void prepare_tea() {
+		
+		System.out.println("Lemon Tea is prepared");
+	
+	}
+	
 }
 
 public class TeaMaker{
 	
-	
+	public static void main(String[] args){
+		
+		tea T = new Lemontea();
+		
+		T.prepare_tea();
+		
+	}
 	
 }
