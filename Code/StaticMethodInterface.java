8ad177802ebf6f9bd0c35f78ce2749da5b95ff27
DhirManish <manish.dhir4@gmail.com>
2018-08-14 00:24:25 +0530

    Static method in interface

---
+++ Code/StaticMethodInterface.java
@@ -0,0 +1,36 @@
+interface staticmethod{
+
+	void printmsg();
+	
+	static int square(int x) {
+
+		return x * x;
+		
+	}
+
+}
+
+class message implements staticmethod{
+	
+	public void printmsg(){
+		
+		System.out.println("Square ");
+	
+	}
+	
+}
+
+public class StaticMethodInterface{
+	
+	public static void main(String[] args){
+	
+		staticmethod msg = new message();
+	
+		msg.printmsg();
+		
+		System.out.println(staticmethod.square(2));
+	
+	}
+	
+	
+}
