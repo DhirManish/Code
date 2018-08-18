4f6279aac48faf9cf3f9d9c60d0c00b502b24654
DhirManish <manish.dhir4@gmail.com>
2018-08-19 00:16:04 +0530

    Assigining a new value to final variable

--- Code/FinalClass.java
+++ Code/FinalClass.java
@@ -3,7 +3,20 @@
 class FinalKey {
 
 	final int value = 1;
-
+	
+	//trying to reinitialize the value keyword
+	
+	void reinit(){
+		
+		value = 2;
+		
+	}
+	
+	//FinalClass.java:11: error: cannot assign a value to final variable value
+    //            value = 2;
+    //            ^
+	//1 error
+	
 }
 
 public class FinalClass {
@@ -13,6 +26,8 @@ public class FinalClass {
 		FinalKey K = new FinalKey();
 		
 		System.out.println(K.value);
+		
+		K.reinit();
 	
 	}
 
