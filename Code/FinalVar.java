9aa791e1ada346c9f3ecccc2c6811c3e0c7302d3
DhirManish <manish.dhir4@gmail.com>
2018-08-21 00:20:16 +0530

    Added results , cannot initailize the final variable in user function

--- Code/FinalVar.java
+++ Code/FinalVar.java
@@ -3,6 +3,13 @@ class Uninit_Var_Name {
 	final String string_name;
 
 
+	//results 
+	
+	//FinalVar.java:7: error: cannot assign a value to final variable string_name
+    //            string_name = "Hello";
+    //            ^
+	//1 error
+	
 	void message(){
 	
 		string_name = "Hello";
