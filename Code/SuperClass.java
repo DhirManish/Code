7a8af42d2745fad33b042a87b8036cdfde4c3b3b
DhirManish <manish.dhir4@gmail.com>
2018-08-23 00:11:03 +0530

    Added the code withing classed

--- Code/SuperClass.java
+++ Code/SuperClass.java
@@ -1,11 +1,33 @@
 class SuperKey {
 
+	String color = "Black";
 
-{
+}
+
+class ChildKey extends SuperKey {
+	
+	String color = "White";
+	
+	void Printcolor(){
+		
+		System.out.println(color);
+		
+		System.out.println(Super.color);
+		
+	}
+	
+}
 
 
 public class SuperClass {
 
+	public static void main(String[] args){
+		
+		ChildKey CK = new ChildKey();
+		
+		CK.Printcolor();
+		
+	}
 
 
 }
