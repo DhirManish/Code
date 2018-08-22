13ada54916c8bac91c9bb21fa75aca6571ad4099
DhirManish <manish.dhir4@gmail.com>
2018-08-23 00:34:56 +0530

    Implementation Completed for Super Method

--- Code/MainClass.java
+++ Code/MainClass.java
@@ -1,13 +1,45 @@
 class SuperMethod {
+	
+	void Method1(){
+		
+		System.out.println("Super Class's Method");
+		
+	}
 
+}
 
+class AnotherMethod extends SuperMethod {
+	
+	void Method1() {
+		
+		//System.out.println("Method called from AnotherMethod class");
+		
+	}
+	
+	void AMehtod() {
+		
+		System.out.println("Method Class");
+		
+	}
+	
+	void Method2() {
+		
+		super.Method1();
+		
+		AMehtod();
+		
+	}
+	
 }
 
 public class MainClass {
 
 	public static void main(String[] args){
-	
-	
+		
+		AnotherMethod AM = new AnotherMethod();
+		
+		AM.Method2();
+		
 	}
 
 }
