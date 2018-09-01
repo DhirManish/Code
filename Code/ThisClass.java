3e1c1bb5fedca93d084f8e84571c05ca9bb086e4
DhirManish <manish.dhir4@gmail.com>
2018-09-01 16:53:37 +0530

    use the constructor of parent class by using Super

--- Code/ThisClass.java
+++ Code/ThisClass.java
@@ -1,10 +1,46 @@
 class Superwiththis{
+	
+	int id;
+	
+	String name;
+	
+	Superwiththis(int id, String name){
+		
+		this.id = id;
+		
+		this.name = name;
+		
+	}
 
+}
+
+class SubClass extends Superwiththis{
+	
+	float salary;
+	
+	SubClass(int id, String name,float salary){
+		
+		super(id,name);
+		
+		this.salary = salary;
+		
+	}
+	
+	void display(){
+		
+		System.out.println(id+" "+name+" "+salary);
+		
+	}
+	
 }
 
 public class ThisClass{
 
 	public static void main(String[] args){
+		
+		SubClass SC = new SubClass(1,"Manish",23f);
+		
+		SC.display();
 		
 		
 	}
