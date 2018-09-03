034da146758b8b008ebd6db8c0811def50aa7a10
DhirManish <manish.dhir4@gmail.com>
2018-09-03 23:24:35 +0530

    Initialize the value in instance initialize block

---
+++ Code/InstanceInit.java
@@ -0,0 +1,25 @@
+class Instance{
+
+	int i;
+
+	Instance(){
+	
+		System.out.println("Value of i : " + i);
+		
+	}
+		
+	{
+		i = 100;
+	}
+
+}	
+
+public class InstanceInit{
+	
+	public static void main(String[] args){
+		
+		Instance inst = new Instance();
+		
+	}
+	
+}
