270d884385c67f472f464589365f6986e32a04e7
DhirManish <manish.dhir4@gmail.com>
2018-10-06 00:32:08 +0530

    Reverse of string using String Builder

---
+++ Code/StringBuilderSB.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+
+public class StringBuilderSB{
+
+	public static void main(String[] args){
+	
+		StringBuilder str  = new StringBuilder("Test");
+		
+		System.out.println("String " + str);
+		
+		System.out.println("Reversed as " + str.reverse());
+		
+	}
+	
+}
